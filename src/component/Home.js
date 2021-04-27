e79347722e3c0ec8f4efce2229ed8ec1a01f15fc
rakeshranjan530 <dashingranjan1213@gmail.com>
2021-04-26 17:38:54 -0700

    Update some style

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,7 @@ import Card from "./Card"
 
 const Home =()=>{
     const details = JSON.parse(localStorage.getItem("data")) || [];
+    if(details.length === 0) return <div style={{textAlign:"center"}}><h3>No Data add some data in ManageProducts</h3></div>
     return(
         <div className="container">
             <div className="Section">
